c03305a9121431d2dc2219a09416caa01c11673d
mokemoko <mokemoko331@gmail.com>
2016-05-03 13:19:39 +0900

    add files

---
+++ python/reset.py
@@ -0,0 +1,7 @@
+from mcpi import minecraft
+
+mc = minecraft.Minecraft.create()
+
+mc.setBlocks(-100,-1,-100,100,30,100,0)
+
+mc.player.setPos(0.0, 0.0, 0.0)
---
+++ python/start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+C:\Python27\python.exe traffic1.py
+PAUSE
